34b7d096e1c35de35b2f9a8d083958837bd966a3
Fabio Sperone <fabios-95@live.it>
2018-11-20 20:12:19 +0100

    Second commit

--- src/test/java/com/unical/ingsw/Esercitazione/AppTest.java
+++ src/test/java/com/unical/ingsw/Esercitazione/AppTest.java
@@ -21,8 +21,14 @@ public class AppTest {
 		Assert.assertEquals(10, app.sum(5, 5));
 	}
 	
+	@Test
+	public void divisionFirstTest() {
+		System.out.println("Test della divisione.");
+		Assert.assertEquals(2, app.division(4, 2));
+	}
+	
 	@Test(expected=ArithmeticException.class)
-	public void divisionTest() {
+	public void divisionSecondTest() {
 		System.out.println("Test divisione per 0.");
 		app.division(5, 0);
 	}
